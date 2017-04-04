4d01ff4445ce4d71a6b7420aedbb1a8f7f535644
aobligacion <airomobligacion@gmail.com>
2017-04-03 21:46:45 -0700

    updated indes.js

--- js/index.js
+++ js/index.js
@@ -24,6 +24,21 @@ $('body').attr("class", "austin");
 console.log("city-type", city);
 }
 
+else if ( city == "LOS ANGELES " || city == "LA" || city == "LAX" ){
+$('body').attr("class", "la");
+console.log("city-type", city);
+}
+
+else if ( city == "SAN FRANCISCO " || city == "SF" || city == "BAY AREA" ){
+$('body').attr("class", "sf");
+console.log("city-type", city);
+}
+
+else if ( city == "SYDNEY " || city == "SYD" ){
+$('body').attr("class", "sydney");
+console.log("city-type", city);
+}
+
 else {
   $('body').attr("class", "default");
 }
